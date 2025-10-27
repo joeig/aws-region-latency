6c532c426ed7fd9e060724b1476d17f4c83772c0
Johannes Eiglsperger <je@eigsys.de>
2025-10-27 09:22:24 +0100

    Support European Sovereign Cloud

--- src/defaultSettings.test.ts
+++ src/defaultSettings.test.ts
@@ -1,14 +1,20 @@
 import { expect, test } from 'vitest';
-import { formatRegionEndpointUrl } from './defaultSettings';
+import { buildRegionEndpointUrl } from './defaultSettings';
 
 test('FORMAT_REGION_ENDPOINT_URL returns the correct region endpoint URL', () => {
-    const result = formatRegionEndpointUrl('eu-west-1');
+    const result = buildRegionEndpointUrl('eu-west-1');
 
     expect(result).toBe('https://dynamodb.eu-west-1.amazonaws.com/ping');
 });
 
 test('FORMAT_REGION_ENDPOINT_URL returns the correct Chinese region endpoint URL', () => {
-    const result = formatRegionEndpointUrl('cn-north-1');
+    const result = buildRegionEndpointUrl('cn-north-1');
 
     expect(result).toBe('https://dynamodb.cn-north-1.amazonaws.com.cn/ping');
+});
+
+test('FORMAT_REGION_ENDPOINT_URL returns the correct European Sovereign Cloud region endpoint URL', () => {
+    const result = buildRegionEndpointUrl('eusc-de-east-1');
+
+    expect(result).toBe('https://dynamodb.eusc-de-east-1.amazonaws.eu/ping');
 });
--- src/defaultSettings.ts
+++ src/defaultSettings.ts
@@ -5,7 +5,21 @@ export const ipRangesUrl = 'https://ip-ranges.amazonaws.com/ip-ranges.json';
 export const fetchTimeout: Millisecond = 10_000;
 export const maxRegionsToTest = 50;
 
-export const formatRegionEndpointUrl: FormatRegionEndpointUrl = (region: string) => {
-    const tld = region.startsWith('cn-') ? 'com.cn' : 'com';
-    return `https://dynamodb.${region}.amazonaws.${tld}/ping`;
+const partitionTlds: Record<string, string> = {
+    cn: 'com.cn',
+    eusc: 'eu'
+} as const;
+
+const findTldForRegion = (region: string): string => {
+    const partition = region.split('-')[0];
+    return partitionTlds[partition] || 'com';
+};
+
+export const buildRegionEndpointUrl: FormatRegionEndpointUrl = (region: string) => {
+    const tld = findTldForRegion(region);
+    const endpoint = new URL('https://localhost');
+    endpoint.hostname = ['dynamodb', region, 'amazonaws', tld].join('.');
+    endpoint.pathname = '/ping';
+
+    return endpoint.toString();
 };
